3fc7df3b2b5329957f94d0176ea57efccfaf7be3
Álvaro Gómez <alvarogomen93@gmail.com>
2022-09-04 02:05:15 +0200

    Update input.tf

--- input.tf
+++ input.tf
@@ -7,6 +7,7 @@ variable "tenant_id" {}
 # Resource Group/Location
 variable "location" {}
 variable "resource_group" {}
+variable "resource_type" {}
 variable "application_type" {}
 # Tags
 variable tier {}
